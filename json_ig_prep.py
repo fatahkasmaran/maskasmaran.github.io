e8a3aefbe697e1bd4b9e2be23ac26d512272225e
maskasmaran <35621823+maskasmaran@users.noreply.github.com>
2021-04-16 15:30:07 +0700

    Update json_ig_prep.py

--- json_ig_prep.py
+++ json_ig_prep.py
@@ -7,19 +7,22 @@ data = pd.read_json('diindonesiaaja.json')
 insta_properties = {}
 with open('diindonesiaaja.csv', 'w') as f:
     csv_writer = DictWriter(f, fieldnames=[
-                            'user_id', 'date', 'caption'])
+                            'user_id', 'date', 'caption', 'url'])
     csv_writer.writeheader()
     for d in data.GraphImages:
-        
+
         # print(d)
-        insta_properties['caption'] = re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", d['edge_media_to_caption']['edges'][0]['node']['text'].lower())
+        # if d['edge_media_to_caption']['edges'] is not False:
+        try:
+            insta_properties['caption'] = re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", d['edge_media_to_caption']['edges'][0]['node']['text'].lower())
+        except:
+            pass
         csv_writer.writerow({
             'user_id': d['id'],
             'date': d['taken_at_timestamp'],
-            'caption': insta_properties['caption']
-#             'comments': d['edge_media_to_comment']['count'],
-#             'height': d['dimensions']['height'],
-#             'width': d['dimensions']['width']
+            'caption': insta_properties['caption'],
+            'url': d['display_url']
+            #             'comments': d['edge_media_to_comment']['count'],
+            #             'height': d['dimensions']['height'],
+            #             'width': d['dimensions']['width']
         })
-
-#, 'comments', 'height', 'width'
