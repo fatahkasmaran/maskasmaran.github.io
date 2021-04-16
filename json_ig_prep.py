eb1c666d0951b818eb799d547398a9bae6cb48ff
maskasmaran <35621823+maskasmaran@users.noreply.github.com>
2021-04-16 13:03:35 +0700

    Update json_ig_prep.py

--- json_ig_prep.py
+++ json_ig_prep.py
@@ -7,7 +7,7 @@ data = pd.read_json('diindonesiaaja.json')
 insta_properties = {}
 with open('diindonesiaaja.csv', 'w') as f:
     csv_writer = DictWriter(f, fieldnames=[
-                            'user_id', 'date', 'caption', 'comments', 'height', 'width'])
+                            'user_id', 'date', 'caption'])
     csv_writer.writeheader()
     for d in data.GraphImages:
         
@@ -16,8 +16,10 @@ with open('diindonesiaaja.csv', 'w') as f:
         csv_writer.writerow({
             'user_id': d['id'],
             'date': d['taken_at_timestamp'],
-            'caption': insta_properties['caption'],
-            'comments': d['edge_media_to_comment']['count'],
-            'height': d['dimensions']['height'],
-            'width': d['dimensions']['width']
+            'caption': insta_properties['caption']
+#             'comments': d['edge_media_to_comment']['count'],
+#             'height': d['dimensions']['height'],
+#             'width': d['dimensions']['width']
         })
+
+#, 'comments', 'height', 'width'
